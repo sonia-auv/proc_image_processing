a0adfe9e98f29eb396c632ca2a2abfa1e736773c
thecanadianroot <tremblay.9.0.0.0@gmail.com>
2021-05-20 15:51:05 -0400

    Added more metrics for benchmark 2

--- tools/benchmarks/benchmark2.cc
+++ tools/benchmarks/benchmark2.cc
@@ -43,30 +43,36 @@ void run(const vector<cv::String> &images) {
     }
     // Benchmark
     chrono::steady_clock::time_point now = chrono::steady_clock::now();
+    int totalTimeResize=0;
+    int totalTimeBlur=0;
+    int totalTimeCanny=0;
     for (const auto &loadedImage : loadedImages) {
         cv::Mat resized, blurred, edges, dest;
 
         chrono::steady_clock::time_point resizeNow = chrono::steady_clock::now();
         resize(loadedImage, resized, cv::Size(1280, 720), 0, 0, cv::INTER_CUBIC);
         chrono::steady_clock::time_point resizeEnd = chrono::steady_clock::now();
+        totalTimeResize+=chrono::duration_cast<chrono::nanoseconds>(resizeEnd - resizeNow).count();
 
         chrono::steady_clock::time_point blurNow = chrono::steady_clock::now();
         blur(resized, blurred, cv::Size(3, 3));
         chrono::steady_clock::time_point blurEnd = chrono::steady_clock::now();
+        totalTimeBlur+=chrono::duration_cast<chrono::nanoseconds>(blurEnd - blurNow).count();
+
 
         chrono::steady_clock::time_point cannyNow = chrono::steady_clock::now();
         Canny(blurred, edges, 100, 100 * 3);
         chrono::steady_clock::time_point cannyEnd = chrono::steady_clock::now();
+        totalTimeCanny+=chrono::duration_cast<chrono::nanoseconds>(cannyEnd - cannyNow).count();
 
         dest = cv::Scalar::all(0);
         resized.copyTo(dest, edges);
-
-        cout << "OpenCV runtime (CPU) - Resize: " << chrono::duration_cast<chrono::nanoseconds>(resizeEnd - resizeNow).count() << " nanoseconds" << endl;
-        cout << "OpenCV runtime (CPU) - Blur: " << chrono::duration_cast<chrono::nanoseconds>(blurEnd - blurNow).count() << " nanoseconds" << endl;
-        cout << "OpenCV runtime (CPU) - Canny: " << chrono::duration_cast<chrono::nanoseconds>(cannyEnd - cannyNow).count() << " nanoseconds" << endl;
     }
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
     cout << "OpenCV runtime (CPU): " << chrono::duration_cast<chrono::milliseconds>(end - now).count() << " milliseconds" << endl;
+    cout << "OpenCV runtime (CPU) - Resize Average: " << totalTimeResize / loadedImages.size() << " nanoseconds" << endl;
+    cout << "OpenCV runtime (CPU) - Blur Average: " << totalTimeBlur / loadedImages.size() << " nanoseconds" << endl;
+    cout << "OpenCV runtime (CPU) - Canny Average: " << totalTimeCanny / loadedImages.size() << " nanoseconds" << endl;
     loadedImages.clear();
 }
 
@@ -84,6 +90,9 @@ void runCUDA(const vector<cv::String> &images) {
     chrono::steady_clock::time_point now = chrono::steady_clock::now();
     cv::Ptr<cv::cuda::Filter> gaussian = cv::cuda::createBoxFilter(0, 0,cv::Size(3,3));
     cv::Ptr<cv::cuda::CannyEdgeDetector> canny = cv::cuda::createCannyEdgeDetector(100.0, 300.0);
+    int totalTimeResize=0;
+    int totalTimeBlur=0;
+    int totalTimeCanny=0;
     for (const auto &loadedImage : loadedImages) {
         cv::cuda::GpuMat src, resized, blurred, edges;
         chrono::steady_clock::time_point uploadNow = chrono::steady_clock::now();
@@ -93,29 +102,29 @@ void runCUDA(const vector<cv::String> &images) {
         chrono::steady_clock::time_point resizeNow = chrono::steady_clock::now();
         cv::cuda::resize(src, resized, cv::Size(1280, 720),0,0,cv::INTER_CUBIC);
         chrono::steady_clock::time_point resizeEnd = chrono::steady_clock::now();
+        totalTimeResize+=chrono::duration_cast<chrono::nanoseconds>(resizeEnd - resizeNow).count();
 
         chrono::steady_clock::time_point blurNow = chrono::steady_clock::now();
         gaussian->apply(resized, blurred);
         chrono::steady_clock::time_point blurEnd = chrono::steady_clock::now();
+        totalTimeBlur+=chrono::duration_cast<chrono::nanoseconds>(blurEnd - blurNow).count();
 
         chrono::steady_clock::time_point cannyNow = chrono::steady_clock::now();
         canny->detect(blurred, edges);
         chrono::steady_clock::time_point cannyEnd = chrono::steady_clock::now();
+        totalTimeCanny+=chrono::duration_cast<chrono::nanoseconds>(cannyEnd - cannyNow).count();
 
         cv::Mat dest;
 
         chrono::steady_clock::time_point downloadNow = chrono::steady_clock::now();
         edges.download(dest);
         chrono::steady_clock::time_point downloadEnd = chrono::steady_clock::now();
-
-        cout << "OpenCV runtime (GPU) - Upload: " << chrono::duration_cast<chrono::nanoseconds>(uploadEnd - uploadNow).count() << " nanoseconds" << endl;
-        cout << "OpenCV runtime (GPU) - Resize: " << chrono::duration_cast<chrono::nanoseconds>(resizeEnd - resizeNow).count() << " nanoseconds" << endl;
-        cout << "OpenCV runtime (GPU) - Blur: " << chrono::duration_cast<chrono::nanoseconds>(blurEnd - blurNow).count() << " nanoseconds" << endl;
-        cout << "OpenCV runtime (GPU) - Canny: " << chrono::duration_cast<chrono::nanoseconds>(cannyEnd - cannyNow).count() << " nanoseconds" << endl;
-        cout << "OpenCV runtime (GPU) - Download: " << chrono::duration_cast<chrono::nanoseconds>(downloadEnd - downloadNow).count() << " nanoseconds" << endl;
     }
     chrono::steady_clock::time_point end = chrono::steady_clock::now();
     cout << "OpenCV runtime (GPU): " << chrono::duration_cast<chrono::milliseconds>(end - now).count() << " milliseconds" << endl;
+    cout << "OpenCV runtime (GPU) - Resize Average: " << totalTimeResize / loadedImages.size() << " nanoseconds" << endl;
+    cout << "OpenCV runtime (GPU) - Blur Average: " << totalTimeBlur / loadedImages.size() << " nanoseconds" << endl;
+    cout << "OpenCV runtime (GPU) - Canny Average: " << totalTimeCanny / loadedImages.size() << " nanoseconds" << endl;
     loadedImages.clear();
 }
 
