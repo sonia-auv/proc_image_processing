1819f9f3656dec5bce5777162c404777d98b0671
thecanadianroot <tremblay.9.0.0.0@gmail.com>
2021-05-20 15:25:30 -0400

    Added benchmarks for CUDA

---
+++ tools/benchmarks/benchmark1.cc
@@ -0,0 +1,117 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <iostream>
+#include <dirent.h>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <opencv2/imgproc.hpp>
+#include <string>
+#include <opencv2/cudaimgproc.hpp>
+#include <map>
+
+using namespace std;
+
+
+map<cv::String, cv::Mat> loadImages(const string &directory) {
+    DIR *dir;
+    map<cv::String, cv::Mat> medias;
+    struct dirent *file;
+    if ((dir = opendir(directory.c_str())) != nullptr) {
+        while ((file = readdir(dir)) != nullptr) {
+            cv::Mat m = imread(directory + "/" + file->d_name, cv::IMREAD_GRAYSCALE);
+            cv::String path = file->d_name;
+            if (file->d_type != DT_DIR && m.data != nullptr){
+                medias.insert(make_pair(path, m));
+            }
+        }
+        closedir(dir);
+    }
+    return medias;
+}
+
+void run(const string &directory) {
+    // Load all in memory to simulate receiving messages from the ROS provider
+    map<cv::String, cv::Mat> loadedImages = loadImages(directory);
+    // Benchmark
+    chrono::steady_clock::time_point now = chrono::steady_clock::now();
+    for (auto &loadedImage : loadedImages) {
+        cv::Mat resized, blurred, edges, dest;
+        resize(loadedImage.second, resized, cv::Size(1280, 720), 0, 0, cv::INTER_CUBIC);
+        blur(resized, blurred, cv::Size(3, 3));
+        Canny(blurred, edges, 100, 100 * 3);
+        dest = cv::Scalar::all(0);
+        resized.copyTo(dest, edges);
+        loadedImage.second = dest;
+    }
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << "OpenCV runtime (CPU): " << chrono::duration_cast<chrono::milliseconds>(end - now).count() << " milliseconds" << endl;
+    cout << "OpenCV runtime (CPU): " << chrono::duration_cast<chrono::nanoseconds>(end - now).count() << " nanoseconds" << endl;
+    for (const auto &out : loadedImages){
+        imwrite("/tmp/cpu/" + out.first, out.second);
+    }
+    loadedImages.clear();
+}
+
+void runCUDA(const string &directory) {
+    shared_ptr<vector<cv::cuda::Stream>> cudaStreams = make_shared<vector<cv::cuda::Stream>>();
+    shared_ptr<vector<cv::cuda::HostMem >> srcMemArray = make_shared<vector<cv::cuda::HostMem >>();
+    shared_ptr<vector<cv::cuda::HostMem >> dstMemArray = make_shared<vector<cv::cuda::HostMem >>();
+    shared_ptr<vector< cv::cuda::GpuMat >> gpuSrcArray = make_shared<vector<cv::cuda::GpuMat>>();
+    shared_ptr<vector< cv::cuda::GpuMat >> gpuDstArray = make_shared<vector<cv::cuda::GpuMat>>();
+    shared_ptr<vector< cv::Mat >> outArray = make_shared<vector<cv::Mat>>();
+    // Load all in memory to simulate receiving messages from the ROS provider
+    map<cv::String, cv::Mat> loadedImages = loadImages(directory);
+    for (const auto &image : loadedImages) {
+        // cout << image << endl;
+        cv::Mat img = imread(directory + "/" + image.first, cv::IMREAD_GRAYSCALE);
+        if (img.data != nullptr) {
+            cv::cuda::Stream s;
+            s.enqueueHostCallback()
+            cudaStreams->emplace_back(s);
+            cv::cuda::GpuMat srcMat;
+            cv::cuda::GpuMat dstMat;
+            cv::Mat outMat;
+            cv::cuda::HostMem srcHostMem = cv::cuda::HostMem(img, cv::cuda::HostMem::PAGE_LOCKED);
+            cv::cuda::HostMem srcDstMem = cv::cuda::HostMem(outMat, cv::cuda::HostMem::PAGE_LOCKED);
+            srcMemArray->emplace_back(srcHostMem);
+            dstMemArray->emplace_back(srcDstMem);
+            gpuSrcArray->emplace_back(srcMat);
+            gpuDstArray->emplace_back(dstMat);
+            outArray->emplace_back(outMat);
+        }
+    }
+    // Benchmark
+    chrono::steady_clock::time_point now = chrono::steady_clock::now();
+    cv::Ptr<cv::cuda::Filter> gaussian = cv::cuda::createBoxFilter(0, 0,cv::Size(3,3));
+    cv::Ptr<cv::cuda::CannyEdgeDetector> canny = cv::cuda::createCannyEdgeDetector(100.0, 300.0);
+    for(int i =0; i < loadedImages.size(); i++){
+        (*gpuSrcArray)[i].upload((*srcMemArray)[i], (*cudaStreams)[i]);
+        cv::cuda::GpuMat resized, blurred, edges;
+        cv::cuda::resize((*gpuSrcArray)[i], resized, cv::Size(1280, 720),0,0,cv::INTER_CUBIC);
+
+        gaussian->apply(resized, blurred);
+
+        canny->detect(blurred, edges);
+        (*gpuDstArray)[i].download((*dstMemArray)[i], (*cudaStreams)[i]);
+        (*outArray)[i]= (*dstMemArray)[i].createMatHeader();
+    }
+    for (int i = 0; i < cudaStreams->size(); i++){
+        (*cudaStreams)[i]..waitForCompletion();
+    }
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << "OpenCV runtime (GPU): " << chrono::duration_cast<chrono::milliseconds>(end - now).count()
+         << " milliseconds" << endl;
+    cout << "OpenCV runtime (GPU): " << chrono::duration_cast<chrono::nanoseconds>(end - now).count() << " nanoseconds"
+         << endl;
+    for (int i =0; i < outArray->size();i++){
+        imwrite("/tmp/cuda/" + to_string(i) + ".png",(*outArray)[i]);
+    }
+    loadedImages.clear();
+}
+
+int start(int argc, char *argv[]) {
+    run("/home/sonia/ros_sonia_ws/src/proc_image_processing/imgs");
+    runCUDA("/home/sonia/ros_sonia_ws/src/proc_image_processing/imgs");
+    return 0;
+}
---
+++ tools/benchmarks/benchmark2.cc
@@ -0,0 +1,123 @@
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <iostream>
+#include <dirent.h>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <opencv2/imgproc.hpp>
+#include <string>
+#include <opencv2/cudaimgproc.hpp>
+
+using namespace std;
+
+vector<cv::String> listFiles(const string &directory) {
+    DIR *dir;
+    vector<cv::String> medias;
+    struct dirent *file;
+    if ((dir = opendir(directory.c_str())) != nullptr) {
+        while ((file = readdir(dir)) != nullptr) {
+            cv::String path = directory + "/" + file->d_name;
+            if (file->d_type != DT_DIR) {
+                medias.emplace_back(path);
+            }
+        }
+        closedir(dir);
+    }
+    return medias;
+}
+
+void run(const vector<cv::String> &images) {
+    // Load all in memory to simulate receiving messages from the ROS provider
+    vector<cv::Mat> loadedImages;
+    for (const auto &image : images) {
+        // cout << image << endl;
+        cv::Mat src = imread(image,  cv::IMREAD_GRAYSCALE);
+        if (src.data != nullptr) {
+            loadedImages.emplace_back(src);
+        }
+    }
+    // Benchmark
+    chrono::steady_clock::time_point now = chrono::steady_clock::now();
+    for (const auto &loadedImage : loadedImages) {
+        cv::Mat resized, blured, edges, dest;
+
+        chrono::steady_clock::time_point resizeNow = chrono::steady_clock::now();
+        resize(loadedImage, resized, cv::Size(1280, 720), 0, 0, cv::INTER_CUBIC);
+        chrono::steady_clock::time_point resizeEnd = chrono::steady_clock::now();
+
+        chrono::steady_clock::time_point blurNow = chrono::steady_clock::now();
+        blur(resized, blured, cv::Size(3, 3));
+        chrono::steady_clock::time_point blurEnd = chrono::steady_clock::now();
+
+        chrono::steady_clock::time_point cannyNow = chrono::steady_clock::now();
+        Canny(blured, edges, 100, 100 * 3);
+        chrono::steady_clock::time_point cannyEnd = chrono::steady_clock::now();
+
+        dest = cv::Scalar::all(0);
+        resized.copyTo(dest, edges);
+
+        cout << "OpenCV runtime (CPU) - Resize: " << chrono::duration_cast<chrono::nanoseconds>(resizeEnd - resizeNow).count() << " nanoseconds" << endl;
+        cout << "OpenCV runtime (CPU) - Blur: " << chrono::duration_cast<chrono::nanoseconds>(blurEnd - blurNow).count() << " nanoseconds" << endl;
+        cout << "OpenCV runtime (CPU) - Canny: " << chrono::duration_cast<chrono::nanoseconds>(cannyEnd - cannyNow).count() << " nanoseconds" << endl;
+    }
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << "OpenCV runtime (CPU): " << chrono::duration_cast<chrono::milliseconds>(end - now).count() << " milliseconds" << endl;
+    loadedImages.clear();
+}
+
+void runCUDA(const vector<cv::String> &images) {
+    // Load all in memory to simulate receiving messages from the ROS provider
+    vector<cv::Mat> loadedImages;
+    for (const auto &image : images) {
+        // cout << image << endl;
+        cv::Mat src = imread(image, cv::IMREAD_GRAYSCALE);
+        if (src.data != nullptr) {
+            loadedImages.emplace_back(src);
+        }
+    }
+    // Benchmark
+    chrono::steady_clock::time_point now = chrono::steady_clock::now();
+    cv::Ptr<cv::cuda::Filter> gaussian = cv::cuda::createBoxFilter(0, 0,cv::Size(3,3));
+    cv::Ptr<cv::cuda::CannyEdgeDetector> canny = cv::cuda::createCannyEdgeDetector(100.0, 300.0);
+    for (const auto &loadedImage : loadedImages) {
+        cv::cuda::GpuMat src, resized, blurred, edges;
+        chrono::steady_clock::time_point uploadNow = chrono::steady_clock::now();
+        src.upload(loadedImage);
+        chrono::steady_clock::time_point uploadEnd = chrono::steady_clock::now();
+
+        chrono::steady_clock::time_point resizeNow = chrono::steady_clock::now();
+        cv::cuda::resize(src, resized, cv::Size(1280, 720),0,0,cv::INTER_CUBIC);
+        chrono::steady_clock::time_point resizeEnd = chrono::steady_clock::now();
+
+        chrono::steady_clock::time_point blurNow = chrono::steady_clock::now();
+        gaussian->apply(resized, blurred);
+        chrono::steady_clock::time_point blurEnd = chrono::steady_clock::now();
+
+        chrono::steady_clock::time_point cannyNow = chrono::steady_clock::now();
+        canny->detect(blurred, edges);
+        chrono::steady_clock::time_point cannyEnd = chrono::steady_clock::now();
+
+        cv::Mat dest;
+
+        chrono::steady_clock::time_point downloadNow = chrono::steady_clock::now();
+        edges.download(dest);
+        chrono::steady_clock::time_point downloadEnd = chrono::steady_clock::now();
+
+        cout << "OpenCV runtime (GPU) - Upload: " << chrono::duration_cast<chrono::nanoseconds>(uploadEnd - uploadNow).count() << " nanoseconds" << endl;
+        cout << "OpenCV runtime (GPU) - Resize: " << chrono::duration_cast<chrono::nanoseconds>(resizeEnd - resizeNow).count() << " nanoseconds" << endl;
+        cout << "OpenCV runtime (GPU) - Blur: " << chrono::duration_cast<chrono::nanoseconds>(blurEnd - blurNow).count() << " nanoseconds" << endl;
+        cout << "OpenCV runtime (GPU) - Canny: " << chrono::duration_cast<chrono::nanoseconds>(cannyEnd - cannyNow).count() << " nanoseconds" << endl;
+        cout << "OpenCV runtime (GPU) - Download: " << chrono::duration_cast<chrono::nanoseconds>(downloadEnd - downloadNow).count() << " nanoseconds" << endl;
+    }
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << "OpenCV runtime (GPU): " << chrono::duration_cast<chrono::milliseconds>(end - now).count() << " milliseconds" << endl;
+    loadedImages.clear();
+}
+
+int main(int argc, char *argv[]) {
+    cv::cuda::printCudaDeviceInfo(cv::cuda::getDevice());
+    run(images);
+    runCUDA(images);
+    return 0;
+}
