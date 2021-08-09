20eaf58d61f42e82ea79ac78db62ee39c0e20bbc
Pierre-Yves V-T <py.vtremblay@gmail.com>
2021-08-09 02:04:44 -0400

    Fixing seg fault and performance for contrast and brightness filter

--- src/proc_image_processing/cpu/filters/low_pass/contrast_and_brightness_filter.h
+++ src/proc_image_processing/cpu/filters/low_pass/contrast_and_brightness_filter.h
@@ -3,8 +3,6 @@
 #ifndef PROC_IMAGE_PROCESSING_FILTERS_CONTRAST_BRIGHTNESS_H_
 #define PROC_IMAGE_PROCESSING_FILTERS_CONTRAST_BRIGHTNESS_H_
 
-#include <proc_image_processing/cpu/algorithm/general_function.h>
-#include <proc_image_processing/cpu/algorithm/parallel_loop_body_wrapper.h>
 #include "proc_image_processing/cpu/filters/filter.h"
 #include <memory>
 
@@ -25,38 +23,10 @@ namespace proc_image_processing {
         ~ContrastAndBrightnessFilter() override = default;
 
         void apply(cv::Mat &image) override {
-            cv::parallel_for_(cv::Range(0, image.rows * image.cols), ParallelCABF(image, contrast_, brightness_));
+            image.convertTo(image, -1, contrast_(), brightness_());
         }
 
     private:
-        class ParallelCABF : public ParallelLoopBodyWrapper {
-        public:
-            explicit ParallelCABF(
-                    cv::Mat &image,
-                    const RangedParameter<double> &contrast,
-                    const RangedParameter<double> &brightness
-            ) : image(image), contrast_(contrast), brightness_(brightness) {}
-
-            ~ParallelCABF() override = default;
-
-            void operator()(const cv::Range &range) const override {
-                for (auto r = range.start; r < range.end; r++) {
-                    int y = r / image.cols;
-                    int x = r % image.cols;
-
-                    auto &vec = const_cast<cv::Vec3b &>(image.at<cv::Vec3b>(y, x));
-                    for (auto c = 0; c < image.channels(); c++) {
-                        vec[c] = cv::saturate_cast<uchar>(contrast_.getValue() * (vec[c]) + brightness_.getValue());
-                    }
-                }
-            }
-
-        private:
-            cv::Mat image;
-            RangedParameter<double> contrast_;
-            RangedParameter<double> brightness_;
-        };
-
         RangedParameter<double> contrast_;
         RangedParameter<double> brightness_;
     };
