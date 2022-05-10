730a2afa5920dd378dfcc37e2e56cecfc10a68e7
0x72d0 <xeaprog@gmail.com>
2022-05-10 17:38:34 -0400

    add try catch message

--- src/proc_image_processing/cpu/server/vision_server.cc
+++ src/proc_image_processing/cpu/server/vision_server.cc
@@ -230,28 +230,42 @@ namespace proc_image_processing {
     ) {
         res.list = "";
 
-        std::string execution_name(req.exec_name), filter_chain_name(req.filterchain);
-        FilterChain::Ptr filter_chain = detection_task_mgr_.getFilterChainFromDetectionTask(execution_name);
+        try
+        {
+            std::string execution_name(req.exec_name), filter_chain_name(req.filterchain);
+            FilterChain::Ptr filter_chain = detection_task_mgr_.getFilterChainFromDetectionTask(execution_name);
 
-        if (filter_chain != nullptr) {
-            try {
-                auto parameters = filter_chain->getFilterParameters(extractFilterIndexFromUIName(req.filter));
-                std::vector<std::string> parameter_names;
-                for (const auto &parameter : parameters) {
-                    parameter_names.push_back(parameter->toString());
+            if (filter_chain != nullptr) {
+                try {
+                    auto parameters = filter_chain->getFilterParameters(extractFilterIndexFromUIName(req.filter));
+                    std::vector<std::string> parameter_names;
+                    for (const auto &parameter : parameters) {
+                        parameter_names.push_back(parameter->toString());
+                    }
+                    res.list = buildRosMessage(parameter_names);
+                    return true;
+                } catch (const std::exception &e) {
+                    ROS_ERROR("Cannot get filter parameters! Cause: %s", e.what());
+                    return false;
                 }
-                res.list = buildRosMessage(parameter_names);
-                return true;
-            } catch (const std::exception &e) {
-                ROS_ERROR("Cannot get filter parameters! Cause: %s", e.what());
-                return false;
             }
+            ROS_ERROR(
+                    "DetectionTask %s does not exist or does not use this filter chain: %s on get filter's param request.",
+                    execution_name.c_str(),
+                    filter_chain_name.c_str()
+            );
         }
-        ROS_ERROR(
-                "DetectionTask %s does not exist or does not use this filter chain: %s on get filter's param request.",
-                execution_name.c_str(),
-                filter_chain_name.c_str()
-        );
+        catch(const std::exception& e)
+        {
+            ROS_ERROR(
+                    "callbackGetFilterAllParam DetectionTask %s crash on %s request with the following exception %s",
+                    req.exec_name.c_str(),
+                    req.filterchain.c_str(),
+                    e.what()
+            );
+        }
+        
+        
         return false;
     }
 
@@ -318,22 +332,36 @@ namespace proc_image_processing {
             sonia_common::SetFilterchainFilterObserver::Request &req,
             sonia_common::SetFilterchainFilterObserver::Response &res
     ) {
-        // For now ignoring filter chain name, but when we will have multiple,
-        // we will have to check the name and find the good filter chain
-        FilterChain::Ptr filter_chain = detection_task_mgr_.getFilterChainFromDetectionTask(req.exec_name);
+        try
+        {
+            // For now ignoring filter chain name, but when we will have multiple,
+            // we will have to check the name and find the good filter chain
+            FilterChain::Ptr filter_chain = detection_task_mgr_.getFilterChainFromDetectionTask(req.exec_name);
 
-        if (filter_chain != nullptr) {
-            res.result = res.SUCCESS;
-            filter_chain->setObserver(extractFilterIndexFromUIName(req.filter));
-            return true;
+            if (filter_chain != nullptr) {
+                res.result = res.SUCCESS;
+                filter_chain->setObserver(extractFilterIndexFromUIName(req.filter));
+                return true;
+            }
+
+            res.result = res.FAIL;
+            ROS_ERROR(
+                    "DetectionTask %s does not exist or does not use this filter chain: %s on get filters request",
+                    req.exec_name.c_str(),
+                    req.filterchain.c_str()
+            );
         }
-
-        res.result = res.FAIL;
-        ROS_ERROR(
-                "DetectionTask %s does not exist or does not use this filter chain: %s on get filters request",
-                req.exec_name.c_str(),
-                req.filterchain.c_str()
-        );
+        catch(const std::exception& e)
+        {
+            ROS_ERROR(
+                    "callbackSetObserver DetectionTask %s crash on %s request with the following exception %s",
+                    req.exec_name.c_str(),
+                    req.filterchain.c_str(),
+                    e.what()
+            );
+        }
+        
+        
         return false;
     }
 
